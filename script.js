6a71ea63b3220b5ea2f63cc27d31f543d85f8848
Abraham Samuel <ebubesammy567@gmail.com>
2024-11-24 04:21:20 +0100

    commit

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function copyMenu() {
 
 copyMenu();
 
-
 // show aside mobile menu
 const menuButton = document.querySelector(".trigger");
 const closeButton = document.querySelector(".t-close");
@@ -31,37 +30,38 @@ closeButton.addEventListener("click", () => {
   addclass.classList.remove("showmenu");
 });
 
-
 document.addEventListener("click", (event) => {
+  // console.log("Clicked element:", event.target);
 
-  console.log("Clicked element:", event.target);
-
-  if (!addclass.contains(event.target) && !menuButton.contains(event.target)) {
-    if(document.body) addclass.classList.remove("showmenu");
+  if (
+    !addclass.contains(event.target) &&
+    !menuButton.contains(event.target)
+  ) {
+    if (document.documentElement && document.querySelector('main')) {
+      // console.log('clicked')
+      addclass.classList.remove("showmenu");
+    }
   }
 });
-      
 
+const submenu = document.querySelectorAll(".site-off .has-child .icon-small.i");
 
-const submenu = document.querySelectorAll(".site-off .has-child .icon-small");
-
+// Attach event listener to each submenu item
 submenu.forEach((menu) => menu.addEventListener("click", toggle));
 
 function toggle(e) {
   e.preventDefault();
 
+  // Collapse all other submenus
   submenu.forEach((item) => {
     if (item !== this) {
       const parent = item.closest(".has-child");
-      
+
       if (parent) {
         parent.classList.remove("expand");
 
         const dropdown = parent.querySelector(":where(ul, .mega)");
         if (dropdown) dropdown.style.maxHeight = "0";
-
-        const iconTurn = parent.querySelector(".icon-small");
-        if (iconTurn) iconTurn.style.transform = "rotate(90deg)";
       }
     }
   });
@@ -74,6 +74,8 @@ function toggle(e) {
 
     if (!parent.classList.contains("expand")) {
       parent.classList.add("expand");
+      // iconAtWomenSection.add("expand")
+
       if (dropdown) dropdown.style.maxHeight = dropdown.scrollHeight + "px";
       if (iconTurn) iconTurn.style.transform = "rotate(270deg)";
     } else {
@@ -81,5 +83,52 @@ function toggle(e) {
       if (dropdown) dropdown.style.maxHeight = "0";
       if (iconTurn) iconTurn.style.transform = "rotate(90deg)";
     }
-  } 
+  }
 }
+
+// Icon thats making me reason my life
+  const submenuTwo = document.querySelector(
+    ".off-canvas nav > ul .fSection .icon-small"
+);
+  submenuTwo.addEventListener('click', toggleTwo)
+
+  
+function toggleTwo(e) {
+  e.preventDefault()
+
+  const parent = document.querySelector(".off-canvas nav > ul .fSection");
+  const dropdown = parent.querySelector(":where(ul, .mega)");
+  
+
+
+  if (parent && dropdown) {
+    if (!parent.classList.contains("expand")) {
+      parent.classList.add("expand");
+
+      dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+      
+        submenuTwo.style.transform = "rotate(180deg)";
+    } else {
+      parent.classList.remove("expand");
+      dropdown.style.maxHeight = "0";
+      
+        submenuTwo.style.transform = "rotate(0deg)";
+    }
+  
+  }
+}
+
+// slider
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 2500, // The time between each slide transition (in milliseconds)
+      disableOnInteraction: false, // Keep autoplay even if the user interacts with the swiper
+    },
+  });
+
+});
